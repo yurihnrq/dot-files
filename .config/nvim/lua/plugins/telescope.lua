e877951643c12d6fb7bdf3450e2cecca30bc4913
Yuri Henrique Bernardes Maciel <35900428+yurihnrq@users.noreply.github.com>
2024-08-01 21:35:22 -0300

    feat: add leader shortcuts for telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,10 +1,13 @@
--- Use telescope with Ctrl+p
-vim.api.nvim_set_keymap("n", "<C-p>", ":Telescope find_files<CR>", {noremap = true, silent = true})
+-- Use telescope with shortcuts
+vim.api.nvim_set_keymap("n", "<leader>ff", ":Telescope find_files<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>fg", ":Telescope live_grep<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>fb", ":Telescope buffers<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>fh", ":Telescope help_tags<CR>", { noremap = true, silent = true })
 
 return {
 	{
 		"nvim-telescope/telescope-fzf-native.nvim",
-    		build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build"
-        },
-        {"nvim-telescope/telescope.nvim", tag = "0.1.6"},
+		build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+	},
+	{ "nvim-telescope/telescope.nvim", tag = "0.1.6" },
 }
