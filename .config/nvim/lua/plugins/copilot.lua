98c80fbc00787a760de3c1f7df53092e4a2a1431
Yuri Henrique Bernardes Maciel <35900428+yurihnrq@users.noreply.github.com>
2024-08-01 21:25:58 -0300

    feat: set copilot suggestions as cmp

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,17 +1,33 @@
--- Toggle Copilot Chat with Ctrl+c
-vim.api.nvim_set_keymap("n", "<C-c>", ":CopilotChatToggle<CR>", {noremap = true, silent = true})
+-- Toggle Copilot Chat with ' cp'
+vim.api.nvim_set_keymap("n", "<leader>cp", ":CopilotChatToggle<CR>", { noremap = true, silent = true })
 
 return {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "canary",
-    dependencies = {
-        {"zbirenbaum/copilot.lua"}, -- or github/copilot.vim
-        {"nvim-lua/plenary.nvim"} -- for curl, log wrapper
-    },
-    opts = {
-        debug = true -- Enable debugging
-        -- See Configuration section for rest
-    }
-    -- See Commands section for default commands if you want to lazy load on them
-}
+	{
 
+		"CopilotC-Nvim/CopilotChat.nvim",
+		branch = "canary",
+		dependencies = {
+			{ "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+			{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+		},
+		opts = {
+			debug = true, -- Enable debugging
+			-- See Configuration section for rest
+		},
+	},
+	{
+		"zbirenbaum/copilot-cmp",
+		opts = {
+			suggestion = { enabled = false },
+			panel = { enabled = false },
+		},
+	},
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+		config = function()
+			require("copilot").setup({})
+		end,
+	},
+}
