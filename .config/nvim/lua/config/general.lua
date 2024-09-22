38a6e39f54d9ae71b5f0aaa6d78e84c15748b213
Yuri Henrique Bernardes Maciel <35900428+yurihnrq@users.noreply.github.com>
2024-09-22 18:38:52 -0300

    chore: set copilot chat window to float mode

--- .config/nvim/lua/config/general.lua
+++ .config/nvim/lua/config/general.lua
@@ -2,6 +2,12 @@
 vim.wo.number = true
 vim.wo.relativenumber = true
 
+-- Change tab size
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
 -- Remove mouse warning in popup menu
 vim.api.nvim_command([[aunmenu PopUp.How-to\ disable\ mouse]])
 vim.api.nvim_command([[aunmenu PopUp.-1-]])
