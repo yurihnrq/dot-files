42473b16edb29e4813b41f34f604ae6e6db4f445
Yuri Henrique B. Maciel <35900428+yurihnrq@users.noreply.github.com>
2024-10-20 20:52:07 -0300

    feat: set new icons for go lang

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -2,34 +2,63 @@
 vim.api.nvim_set_keymap("n", "<C-b>", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
 
 return {
-	"nvim-tree/nvim-tree.lua",
-	lazy = false,
-	dependencies = {
+	{
 		"nvim-tree/nvim-web-devicons",
+		opts = {
+			override = {
+				go = {
+					icon = "󰟓",
+					color = "#00add8",
+					name = "go",
+				},
+				["go.mod"] = {
+					icon = "󰟓",
+					color = "#00add8",
+					name = "go_mod",
+				},
+				["go.sum"] = {
+					icon = "󰟓",
+					color = "#00add8",
+					name = "go_sum",
+				},
+                ["go.work"] = {
+                    icon = "󰟓",
+                    color = "#00add8",
+                    name = "go_work",
+                }
+			},
+		},
 	},
-	opts = {
-		view = {
-			side = "right",
-			width = {
-				min = "25%",
-				max = "35%",
-			},
+	{
+		"nvim-tree/nvim-tree.lua",
+		lazy = false,
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
 		},
-		sync_root_with_cwd = true,
-		update_focused_file = {
-			enable = true,
-			update_root = {
-				enable = true,
+		opts = {
+			view = {
+				side = "right",
+				width = {
+					min = "25%",
+					max = "35%",
+				},
 			},
-		},
-		renderer = {
-			indent_markers = {
+			sync_root_with_cwd = true,
+			update_focused_file = {
 				enable = true,
-				icons = {
-					corner = "└ ",
-					edge = "| ",
-					item = "| ",
-					none = "  ",
+				update_root = {
+					enable = true,
+				},
+			},
+			renderer = {
+				indent_markers = {
+					enable = true,
+					icons = {
+						corner = "└ ",
+						edge = "| ",
+						item = "| ",
+						none = "  ",
+					},
 				},
 			},
 		},
