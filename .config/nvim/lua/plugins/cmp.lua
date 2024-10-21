8b6686bc206f694dd80d4ee9bb0f7e5e436ee733
Yuri Henrique B. Maciel <35900428+yurihnrq@users.noreply.github.com>
2024-10-20 21:38:03 -0300

    feat(nvim): add lspkind plugin

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,43 +1,71 @@
+vim.api.nvim_set_hl(0, "CmpItemKindCopilot", { fg = "#6CC644" })
+
 return {
-	"hrsh7th/nvim-cmp",
-	dependencies = {
-		"neovim/nvim-lspconfig",
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-cmdline",
-		{
-			"L3MON4D3/LuaSnip",
-			dependencies = {
-				"saadparwaiz1/cmp_luasnip",
+	{
+		"onsails/lspkind.nvim",
+		config = function()
+			local lspkind = require("lspkind")
+			lspkind.init({
+				symbol_map = {
+					Copilot = "",
+				},
+			})
+		end,
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"onsails/lspkind.nvim",
+			{
+				"L3MON4D3/LuaSnip",
+				dependencies = {
+					"saadparwaiz1/cmp_luasnip",
+				},
 			},
 		},
-	},
-	opts = function()
-		local cmp = require("cmp")
+		opts = function()
+			local lspkind = require("lspkind")
+			local cmp = require("cmp")
 
-		cmp.setup({
-			snippet = {
-				-- REQUIRED - you must specify a snippet engine
-				expand = function(args)
-					require("luasnip").lsp_expand(args.body)
-				end,
-			},
-			mapping = cmp.mapping.preset.insert({
-				-- ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-				-- ['<C-f>'] = cmp.mapping.scroll_docs(4),
-				["<C-Space>"] = cmp.mapping.complete(),
-				["<C-e>"] = cmp.mapping.abort(),
-				["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-			}),
-			sources = cmp.config.sources({
-				{ name = "copilot" },
-				{ name = "nvim_lsp" },
-				{ name = "luasnip" }, -- For luasnip users.
-				{
-					{ name = "buffer" },
+			cmp.setup({
+				snippet = {
+					-- REQUIRED - you must specify a snippet engine
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
 				},
-			}),
-		})
-	end,
+				mapping = cmp.mapping.preset.insert({
+					-- ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+					-- ['<C-f>'] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+				}),
+				sources = cmp.config.sources({
+					{ name = "copilot" },
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" }, -- For luasnip users.
+					{
+						{ name = "buffer" },
+					},
+				}),
+				formatting = {
+					format = lspkind.cmp_format({
+						mode = "symbol",
+						with_text = true,
+						ellipsis_char = "...",
+						show_labelDetails = true,
+						maxwidth = function()
+							return math.floor(0.45 * vim.o.columns)
+						end,
+					}),
+				},
+			})
+		end,
+	},
 }
