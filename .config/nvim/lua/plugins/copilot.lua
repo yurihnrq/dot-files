12e68dda902185ca7e89d31d1abb1d63eb16ec39
Yuri Henrique B. Maciel <35900428+yurihnrq@users.noreply.github.com>
2024-10-24 09:24:50 -0300

    chore(nvim): disable debug for copilot chat

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -11,8 +11,6 @@ return {
 			{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
 		},
 		opts = {
-			debug = true, -- Enable debugging
-			-- See Configuration section for rest
 			window = {
 				layout = "float",
 			},
