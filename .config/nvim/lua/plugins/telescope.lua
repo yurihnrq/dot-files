7cdb1133e7f8c69c7218e5d22bf487594cc59fc3
Yuri Henrique B. Maciel <35900428+yurihnrq@users.noreply.github.com>
2024-10-24 09:26:46 -0300

    feat(nvim): add file browser extension and improve telescope config

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -6,8 +6,49 @@ vim.api.nvim_set_keymap("n", "<leader>fh", ":Telescope help_tags<CR>", { noremap
 
 return {
 	{
+		"nvim-telescope/telescope-file-browser.nvim",
+		dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+	},
+	{
 		"nvim-telescope/telescope-fzf-native.nvim",
 		build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
 	},
-	{ "nvim-telescope/telescope.nvim", tag = "0.1.6" },
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.6",
+		config = function()
+			require("telescope").setup({
+				defaults = {
+					sorting_strategy = "ascending",
+					layout_config = {
+						horizontal = {
+							prompt_position = "top",
+							preview_width = 0.55,
+						},
+						width = 0.87,
+						height = 0.80,
+					},
+				},
+			})
+			require("telescope").load_extension("file_browser")
+			require("telescope").load_extension("fzf")
+
+			-- Wrap lines in previewer
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "TelescopePreviewerLoaded",
+				callback = function()
+					vim.wo.wrap = true
+				end,
+			})
+
+			-- Open file browser on startup
+			--	vim.api.nvim_create_autocmd("VimEnter", {
+			--		callback = function()
+			--			if vim.fn.argv(0) == "" then
+			--				require("telescope").extensions.file_browser.file_browser()
+			--			end
+			--		end,
+			--	})
+		end,
+	},
 }
