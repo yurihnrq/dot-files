225d1e66693a07b6eeb6a235b713dcc0c15752bf
Yuri Henrique B. Maciel <35900428+yurihnrq@users.noreply.github.com>
2024-10-24 09:23:59 -0300

    feat(nvim): highlight current line and hide numbers on terminal

--- .config/nvim/lua/config/general.lua
+++ .config/nvim/lua/config/general.lua
@@ -1,6 +1,22 @@
+-- Highlight current line
+vim.opt.cursorline = true
+
 -- Add line numbers
 vim.wo.number = true
 vim.wo.relativenumber = true
+
+-- Remove line numbers on TermOpen and add again on TermClose
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+augroup("__term__", { clear = true })
+autocmd("TermOpen", {
+	group = "__term__",
+	command = "setlocal nonumber norelativenumber",
+})
+autocmd("TermClose", {
+	group = "__term__",
+	command = "setlocal number relativenumber",
+})
 
 -- Change tab size
 vim.opt.tabstop = 4
