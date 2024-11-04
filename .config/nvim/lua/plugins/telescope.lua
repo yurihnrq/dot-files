9a8a14058f7144f7c67e03251901c00aba8794e7
Yuri Henrique B. Maciel <35900428+yurihnrq@users.noreply.github.com>
2024-11-04 09:24:56 -0300

    feat: add telescope resume shortcut

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -3,6 +3,8 @@ vim.api.nvim_set_keymap("n", "<leader>ff", ":Telescope find_files<CR>", { norema
 vim.api.nvim_set_keymap("n", "<leader>fg", ":Telescope live_grep<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>fb", ":Telescope buffers<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>fh", ":Telescope help_tags<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>fr", ":Telescope resume<CR>", { noremap = true, silent = true })
+
 
 return {
 	{
