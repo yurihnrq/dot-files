f74e5348903a60d5abb6051523ca474819a1e6e3
Yuri Henrique B. Maciel <35900428+yurihnrq@users.noreply.github.com>
2024-11-07 23:03:23 -0300

    chore: change tree plugin to nvim-tree

---
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,66 @@
+-- Toggle Nvim Tree with Ctrl+b
+vim.api.nvim_set_keymap("n", "<C-b>", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
+
+return {
+	{
+		"nvim-tree/nvim-web-devicons",
+		opts = {
+			override = {
+				go = {
+					icon = "󰟓",
+					color = "#00add8",
+					name = "go",
+				},
+				["go.mod"] = {
+					icon = "󰟓",
+					color = "#00add8",
+					name = "go_mod",
+				},
+				["go.sum"] = {
+					icon = "󰟓",
+					color = "#00add8",
+					name = "go_sum",
+				},
+				["go.work"] = {
+					icon = "󰟓",
+					color = "#00add8",
+					name = "go_work",
+				},
+			},
+		},
+	},
+	{
+		"nvim-tree/nvim-tree.lua",
+		lazy = false,
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		opts = {
+			view = {
+				side = "right",
+				width = {
+					min = "25%",
+					max = "35%",
+				},
+			},
+			sync_root_with_cwd = true,
+			update_focused_file = {
+				enable = true,
+				update_root = {
+					enable = true,
+				},
+			},
+			renderer = {
+				indent_markers = {
+					enable = true,
+					icons = {
+						corner = "└ ",
+						edge = "| ",
+						item = "| ",
+						none = "  ",
+					},
+				},
+			},
+		},
+	},
+}
